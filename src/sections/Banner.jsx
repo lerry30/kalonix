47a4fa676eba4795521b708ca8d36a52454f99c1
John Lerry Ramos Samson <96895997+lerry30@users.noreply.github.com>
2025-11-13 07:37:12 +0800

    Update Banner.jsx

--- src/sections/Banner.jsx
+++ src/sections/Banner.jsx
@@ -3,7 +3,7 @@ import CTAButton from '../components/CTAButton';
 const Banner = () => {
 
   return (
-    <section className="w-full h-[calc(100vh-var(--navbar-height))] flex justify-center items-center flex-col">
+    <section className="w-full init_sec_center flex justify-center items-center flex-col">
       <h1 className="text-6xl text-center font-bold">Top Wireless Earbuds</h1>
       <h2 className="text-5xl text-center font-semibold mb-4">Under $50 in 2025</h2>
       <p className="mb-6 text-center">Discover budget-friendly earbuds with great sound & features.</p>
